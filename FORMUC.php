782a341f4dff0a1fb725b036f274dceba6b6c4da
nicolas433 <ngrisoste@gmail.com>
2019-09-17 11:46:07 -0300

    Commit do almoço

--- FORMUC.php
+++ FORMUC.php
@@ -13,62 +13,6 @@
     $sql_query = "SELECT COLUMN_NAME, DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = '$tabela'";
     $q = $conexao->prepare($sql_query);
 
-    if($q->execute()){
-        while($linha = $q->fetch(PDO::FETCH_ASSOC)){
-            print_r($linha);
-            echo "</br>";
-        }
-    }
-
-    /*
-    $gg = [];
-    $gg[0] = "teste";
-    $data_type;
-    $gambcont = 1;
-    $primeiro_elemento;
-
-
-    if($q->execute()){
-        while($linha = $q->fetch(PDO::FETCH_ASSOC)){
-            foreach($linha as $chave => $valor){
-                if($gambcont == 1){
-                    $primeiro_elemento = $chave;
-                    $gambcont++;
-                    $gg[0] = $chave; 
-                }
-                if($chave == $primeiro_elemento and $gambcont > 2){
-                    echo "saiu@!!@@";
-                    break;
-                }else{                  
-                    $gambcont ++;
-                    if($gambcont>3){
-                        array_push($gg, $chave);
-                    }
-                }
-            }
-            break;
-        }
-    }
-    */
-
-
-
-    //SELECT DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = 'aluguel' AND COLUMN_NAME = 'aluguel_id'
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
 ?>
 <html>
     <head>
@@ -82,34 +26,23 @@
 
 
         <form method='POST' action='addCliente.php'>
-            <p>Id da loja:</p>
-            <label for="um">Um</label>
-            <input type="radio" name="loja_id" id="um" value="1">
-            <label for="dois">Dois</label>
-            <input type="radio" name="loja_id" id="dois" value="2">
-            <br>
-            <br>
+            <?php
+                if($q->execute()){
+                    while($linha = $q->fetch(PDO::FETCH_ASSOC)){
+                        if(
+                           $linha['COLUMN_NAME']!= "data_criacao" and
+                           $linha['COLUMN_NAME']!= "ultima_atualizacao" and
+                           $linha['COLUMN_NAME']!= "ativo"){
+                               echo "<label for=".$linha['COLUMN_NAME'].">".$linha['COLUMN_NAME'].": 
+                               <input type='text' class='form-control form-control-lg' name=".$linha['COLUMN_NAME']."/>" ;
+                               echo "</br>";
+                           }
 
-            <label for="primeiro_nome">primeiro_nome</label>
-            <input type="text" name="primeiro_nome" id="primeiro_nome">
-            <br>
+                    }
+                }
 
-            <label for="ultimo_nome">ultimo_nome</label>
-            <input type="text" name="ultimo_nome" id="ultimo_nome">
-            <br>
 
-            <label for="email">email</label>
-            <input type="email" name="email" id="email">
-            <br>
-
-            <!-- tenho que arrumar um create pra endereco_id tb -->
-            <label for="endereco_id">Mah, se tu já tiver teu <br>
-                            endereço cadastrado e souber <br> 
-                            o id dele no BD, bota ai ashuashusa <br>
-                            (se não, deixa em branco)</label>
-            <input type="text" name="endereco_id" id="endereco_id">
-            <br>
-
+            ?>
             <input type='submit' value='CREATE'>
         </form>
     </body>
